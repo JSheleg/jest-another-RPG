ba184379f05b0dfc5e54c0292172a68953b33aff
JSheleg <jessica.sheleg@gmail.com>
2021-07-08 16:54:53 -0500

    player inventory

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,15 +1,22 @@
+
+jest.mock('../lib/Potion.js');
 const Potion = require('../lib/Potion');
-jest.mock('../lib/Potion');
-console.log(new Potion());
+const PotionMock = require('../lib/_mocks_/Potion.js');
+// console.log(Potion)
+// const PotionTest = jest.spyOn(Potion)
+Potion.mockImplementation(() => PotionMock());
 
 
 const Player = require('../lib/Player');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
-
+    console.log(Potion());
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
